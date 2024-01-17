e5971759d6cff60bd44692e3774004aa6c777a2c
bhthong20 <122037194+bhthong20@users.noreply.github.com>
2024-01-17 16:48:48 +0700

    Test2
    Ver2

---
+++ webbangiay/src/main/java/com/example/demo/services/impl/HinhAnhServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.services.impl;
+
+import com.example.demo.services.HinhAnhService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HinhAnhServiceImpl implements HinhAnhService {
+
+
+}
