b4d819added35cc34b648ee6441a3a4a074a5f3e
bhthong20 <122037194+bhthong20@users.noreply.github.com>
2024-01-19 19:46:36 +0700

    CRUD
    Nhóm trưởng

--- webbangiay/src/main/java/com/example/demo/services/ChatLieuService.java
+++ webbangiay/src/main/java/com/example/demo/services/ChatLieuService.java
@@ -1,6 +1,12 @@
 package com.example.demo.services;
 
+import com.example.demo.models.ChatLieu;
+
+import java.util.List;
+
 public interface ChatLieuService {
 
+    public List<ChatLieu> findAll();
 
+    public ChatLieu add(ChatLieu chatLieu);
 }
--- webbangiay/src/main/java/com/example/demo/services/HinhAnhService.java
+++ webbangiay/src/main/java/com/example/demo/services/HinhAnhService.java
@@ -1,6 +1,10 @@
 package com.example.demo.services;
 
+import com.example.demo.models.HinhAnh;
+
+import java.util.List;
+
 public interface HinhAnhService {
 
-
+    public List<HinhAnh> findAll();
 }
