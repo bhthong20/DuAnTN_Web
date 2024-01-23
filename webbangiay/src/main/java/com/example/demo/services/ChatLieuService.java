6b847bbcca189480d9eaef5603713c536ee144f8
bhthong20 <122037194+bhthong20@users.noreply.github.com>
2024-01-23 19:31:25 +0700

    Update

---
+++ webbangiay/src/main/java/com/example/demo/services/ChatLieuService.java
@@ -0,0 +1,24 @@
+package com.example.demo.services;
+
+import com.example.demo.models.ChatLieu;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ChatLieuService {
+    public Page<ChatLieu> getAll(Pageable pageable);
+
+    public List<ChatLieu> findAll();
+
+    public List<ChatLieu> search(String search);
+
+    public ChatLieu findById(UUID id);
+
+    public ChatLieu add(ChatLieu chatLieu);
+
+    public ChatLieu update(UUID id, ChatLieu chatLieu);
+
+    public Boolean delete(UUID id);
+}
---
+++ webbangiay/src/main/java/com/example/demo/services/impl/ChucVuServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.demo.services.impl;
+
+import com.example.demo.services.ChucVuService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChucVuServiceImpl implements ChucVuService {
+
+}
