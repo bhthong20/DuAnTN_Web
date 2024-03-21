da2babf26911cfee509588fa43e11b4da311e984
Duchuy <duchuy22x@gmail.com>
2024-03-21 21:18:05 +0700

    fix lai dia chi

--- webbangiay/src/main/java/com/example/demo/controllers/DiaChiController.java
+++ webbangiay/src/main/java/com/example/demo/controllers/DiaChiController.java
@@ -4,9 +4,12 @@ import com.example.demo.models.ChatLieu;
 import com.example.demo.models.ChiTietSanPham;
 import com.example.demo.models.ChucVu;
 import com.example.demo.models.DiaChi;
+import com.example.demo.models.HoaDon;
 import com.example.demo.models.KhachHang;
+import com.example.demo.models.KhuyenMai;
 import com.example.demo.models.KichThuoc;
 import com.example.demo.models.MauSac;
+import com.example.demo.models.NhanVien;
 import com.example.demo.services.ChucVuService;
 import com.example.demo.services.DiaChiService;
 import com.example.demo.services.KhachHangService;
@@ -27,6 +30,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -50,7 +54,7 @@ public class DiaChiController {
         model.addAttribute("diaChi", list.getContent());
         model.addAttribute("total", list.getTotalPages());
         model.addAttribute("contentPage", "dia-chi/hien-thi.jsp");
-        return "layout";
+        return "dia-chi/hien-thi";
     }
 
     @GetMapping("/dia-chi/delete/{id}")
@@ -59,56 +63,48 @@ public class DiaChiController {
         return "redirect:/hien-thi/dia-chi";
     }
 
-//    @GetMapping("/view-update/{id}")
-//    public String viewUpdate(Model model, @PathVariable("id") UUID id) {
-//        ChucVu cv = chucVuService.findById(id);
-//        model.addAttribute("chucVu", cv);;
-//        model.addAttribute("contentPage", "/chuc-vu/update.jsp");
-//        return "/chuc-vu/update";
-//    }
-//
-//    @PostMapping("update/{id}")
-//    public String update(@ModelAttribute(name = "nhanVien") NhanVien nhanVien,
-//                         @PathVariable(name = "id") UUID id ,@ModelAttribute("chucVu") ChucVu chucVu) {
-//        NhanVien nv = nhanVienService.findById(id);
-//        nhanVien.setMa(nv.getMa());
-//        nhanVien.setHoTen(nv.getHoTen());
-//        nhanVien.setEmail(nv.getEmail());
-//        nhanVien.setSdt(nv.getSdt());
-//        nhanVien.setNgaySinh(nv.getNgaySinh());
-//        nhanVien.setGioiTinh(nv.getGioiTinh());
-//        nhanVien.setDiaChi(nv.getDiaChi());
-//        nhanVien.setCanCuoc(nv.getCanCuoc());
-//        nhanVien.setTaiKhoan(nv.getTaiKhoan());
-//        nhanVien.setMatKhau(nv.getMatKhau());
-//        nhanVien.setNgayCapNhat(Date.valueOf(LocalDate.now()));
-//        nhanVien.setTinhTrang(nv.getTinhTrang());
-//        nhanVienService.update(id, nhanVien);
-//        return "redirect:/hien-thi/nhan-vien";
-//    }
-//
+    @GetMapping("/dia-chi/view-update/{id}")
+    public String viewUpdate(Model model, @PathVariable("id") UUID id,
+                             @ModelAttribute(name = "diaChi") DiaChi diaChi,
+                             @ModelAttribute(name = "khachHang") KhachHang khachHang) {
+        List<KhachHang> listKh = khachHangService.findAll();
+        model.addAttribute("listKh", listKh);
+        DiaChi dc = diaChiService.findById(id);
+        model.addAttribute("diaChi", dc);
+        model.addAttribute("contentPage", "/dia-chi/update.jsp");
+        return "/dia-chi/update";
+    }
+
+
+    @PostMapping("/dia-chi/update/{id}")
+    public String update(@ModelAttribute(name = "diaChi") DiaChi diaChi,
+                         @PathVariable(name = "id") UUID id, @ModelAttribute("khachHang") KhachHang khachHang) {
+        DiaChi nv = diaChiService.findById(id);
+        diaChi.setNgayCapNhat(Date.valueOf(LocalDate.now()));
+        System.out.println(nv.toString());
+        diaChiService.update(id, diaChi);
+        return "redirect:/hien-thi/dia-chi";
+    }
 
     @GetMapping("/dia-chi/view-add")
     public String viewAdd(Model model, @ModelAttribute("diaChi") DiaChi diaChi,
-                          @ModelAttribute ("khachHang")KhachHang khachHang) {
+                          @ModelAttribute("khachHang") KhachHang khachHang) {
+        List<KhachHang> listKh = khachHangService.findAll();
         model.addAttribute("chucVu", new ChucVu());
-        model.addAttribute("listKh" , khachHangService.findAll());
-//        model.addAttribute("listCv", chucVuService.getAll());
+        model.addAttribute("listKh", listKh);
         model.addAttribute("contentPage", "dia-chi/add.jsp");
         return "dia-chi/add";
     }
-
 
     @PostMapping("/dia-chi/add")
     public String add(Model model, @ModelAttribute("diaChi") DiaChi diaChi,
                       @ModelAttribute("khachHang") KhachHang khachHang, BindingResult result) {
         if (result.hasErrors()) {
-            model.addAttribute("listKh" , khachHangService.findAll());
+            model.addAttribute("listKh", khachHangService.findAll());
             model.addAttribute("contentPage", "dia-chi/add.jsp");
             return "dia-chi/add";
         }
-//        String maDiaChi = "DC00" + (diaChiService.getAll().size() + 1);
-//        diaChi.s;
+        diaChi.setId(UUID.randomUUID());
         diaChi.setNgayTao(Date.valueOf(LocalDate.now()));
         diaChiService.add(diaChi);
         System.out.println("listChucVu");
